255cf38b32b832d2aa2a9a08b74788535b1104aa
Jeus.Han <rossoneri1012@gmail.com>
2022-03-24 16:51:18 +0900

    Time: 119 ms (9.93%), Space: 41.8 MB (72.85%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.js
+++ 1025-divisor-game/1025-divisor-game.js
@@ -4,10 +4,12 @@
  */
 var divisorGame = function(n) {
     //
-    let result = false;
     let dp = [0];
     
     for(let i = 1; i <= n; i++) { 
+        // alice, bob은 1을 번갈아가며 선택한다는 가정으로 계산
+        // n값이 동일해도 alice, bob이 1 이외의 숫자를 선택시 결과가 true/false가 
+        // 다르게 결과가 나오는 상황이 발생
         dp[i] = dp[i-1] +1
     }
     
