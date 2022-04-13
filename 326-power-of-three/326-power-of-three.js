909c9b08c475b61a8f0ff330365dce96c0a71445
Jeus.Han <rossoneri1012@gmail.com>
2022-04-13 18:30:35 +0900

    Time: 188 ms (89.81%), Space: 51.6 MB (30.12%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    //
+    while(n>1) {
+        if(n%3!=0) return false;
+        n /= 3;
+    }
+    return n<=0 ? false : true;
+};
